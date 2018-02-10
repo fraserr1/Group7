f648efbee804c175c0311f6ec8b6688f287deda1
Fraser Raney <fraserraney@trentu.ca>
2018-02-10 13:15:45 -0500

    testing JavaFX: switching scence with button

--- myTestProject/src/Main.java
+++ myTestProject/src/Main.java
@@ -1,10 +1,7 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
