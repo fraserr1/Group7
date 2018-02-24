2f089f94a010ef0cdb9be5b30a3dfeb81eb3622e
Fraser Raney <fraserraney@trentu.ca>
2018-02-23 23:45:59 -0500

    Added Test class

--- Assignment2P1/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[]args){
-        System.out.println("Hello Assignment 2 Part 1");
-        System.out.println("resolved");
-    }
-}
---
+++ Assignment2P1/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[]args){
+        GeometricObject gObjectArray [] = new GeometricObject [4];
+        // TODO: Complete your code here
+    }
+
+    private static void printAreaAndPerimeter(GeometricObject gObject){
+        //TODO: Complete your code here
+    }
+}
