c90031500d6e9fb9257897c6c08d70db1e2c0c4c
Fraser Raney <fraserraney@trentu.ca>
2018-04-05 02:53:09 -0400

    cleaning up

--- Assignment2P1/src/EquilateralTriangle.java
+++
@@ -1,9 +0,0 @@
-public class EquilateralTriangle extends Triangle {
-    private double side;
-
-    /** Constructor */
-    public EquilateralTriangle(double side) {
-        super(side, side, side);
-        this.side = side;
-    }
-}
